b4316d5a66ddbffe1b64c6fcbac3e4f4342a25f1
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 19:48:40 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name]
-    sessions[:name] = params[:name]
-    redirect_to {controller: 'application', 'hello'}
-  else
-    redirect_to {controller: 'sessions', action: 'new'}
-
-
-  end
+      if params[:name]
+      sessions[:name] = params[:name]
+      redirect_to {controller: 'application', 'hello'}
+    else
+      redirect_to {controller: 'sessions', action: 'new'}
+    end 
+  end 
 
   def destroy
     session.destroy :name
