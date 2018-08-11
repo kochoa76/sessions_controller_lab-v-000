8326dfbc22150d37a5755de0a3ac980bedfa4066
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 19:54:06 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if params[:name]== "" || !params[:name]
       redirect_to controller: 'sessions', action: 'new'
     else
-      sessions[:name] = params[:name]
+      session[:name] = params[:name]
       redirect_to controller: 'application', action: 'hello'
     end
   end
