77f17c4bc23c2b971a63bdcbd4b31a99dffff990
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 19:43:03 +0000

    automatically backed up by learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    
+    sessions[:name] = params[:name]
+     
+      
+  end
+
+  def destroy
+    session.destroy :name 
+    redirect_to {controller: 'application', action: 'hello'}
+    
+  end
+
+  
+end
