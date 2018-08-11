65a6815e5ccc4d062ad2b3152f1b69d88ddf68d7
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 19:54:40 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.destroy :name
+    session.destroy 
     redirect_to controller:'application', action: 'hello'
 
   end
